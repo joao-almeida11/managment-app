ea0011f3351b8dcc94bf182839e18d51ade502ef
Joao Almeida <joao-almeida11@outlook.com>
2025-11-11 12:31:01 +0000

    todo: endpoints list

---
+++ src/api/controllers/auth/index.ts
@@ -0,0 +1,10 @@
+// TODO POST /api/auth/register → create user
+// TODO POST /api/auth/login → return JWT
+// TODO export { default as changePassword } from './auth/change-password.js';
+// TODO export { default as register } from './auth/register.js';
+// TODO export { default as login } from './auth/login.js';
+// TODO export { default as logout } from './auth/logout.js';
+// TODO export { default as verifyEmail } from './auth/verify-email.js';
+// TODO export { default as refreshToken } from './auth/refresh-token.js';
+// TODO export { default as forgotPassword } from './auth/forgot-password.js';
+// TODO export { default as sendVerificationCode } from './auth/send-verification-code.js';
---
+++ src/api/controllers/user/index.ts
@@ -0,0 +1,6 @@
+// // EDIT
+// TODO export { default as editUser } from './edit/edit-user.js';
+
+// // OTHER
+// TODO export { default as getUser } from './get-user.js';
+// TODO export { default as deleteUser } from './delete-user.js';
