40fe93c5f578ebaf7e01b51c6cfd72c08526e5a5
Joao Almeida <joao-almeida11@outlook.com>
2025-11-27 04:39:29 +0000

    fix: env

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,13 +3,12 @@ import "dotenv/config";
 import { z } from "zod";
 
 const envSchema = z.object({
-  NODE_ENV: z
-    .enum(["development", "production", "test"])
-    .default("development"),
+  NODE_ENV: z.enum(["development", "production", "test"]),
   DATABASE_URL: z.url(),
   BCRYPT_ENCRYPTION_SALT_ROUNDS: z.enum(["10", "12"]).default("12"),
   JWT_SECRET: z.string().min(6),
   PORT: z.enum(["3000", "3001"]).default("3001"),
+  LOG_LEVEL: z.enum(["info"]),
 });
 
 export type Env = z.infer<typeof envSchema>;
@@ -24,3 +23,4 @@ export const NODE_ENV: Env["NODE_ENV"] = env.NODE_ENV;
 export const PORT: Env["PORT"] = env.PORT;
 export const BCRYPT_ENCRYPTION_SALT_ROUNDS: Env["BCRYPT_ENCRYPTION_SALT_ROUNDS"] =
   env.BCRYPT_ENCRYPTION_SALT_ROUNDS;
+export const LOG_LEVEL: Env["LOG_LEVEL"] = env.LOG_LEVEL;
